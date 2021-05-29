e6e24a71c1de5de5c2c18841f6cb73e3c86fe498
FLUM_AVES <1060464645@qq.com>
2021-05-29 10:37:50 +0800

    修改visitorView

--- Controllers/VisitorView/VisitorViewController.m
+++ Controllers/VisitorView/VisitorViewController.m
@@ -23,14 +23,32 @@
         UIView *view = [[UIView alloc] initWithFrame:self.view.bounds];
         self.view = view;
         self.view.backgroundColor = [UIColor whiteColor];
+    
+    //图标
+    CGFloat imgW = 245;
+    CGFloat imgH = 115;
+    CGFloat imgX = ([UIScreen mainScreen].bounds.size.width - imgW) / 2;
+    CGFloat imgY = [UIScreen mainScreen].bounds.size.width - imgH - 50;
+    
+    UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(imgX, imgY, imgW, imgH)];
+    imgView.image = [UIImage imageNamed:@"首页"];
+    
+    [self.view addSubview:imgView];
 
-        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(150, 200, 100, 30)];
-        [btn setTitleColor: [UIColor orangeColor] forState:UIControlStateNormal];
-        [btn setTitleColor: [UIColor grayColor] forState:UIControlStateHighlighted];
-        [btn setTitle:@"登陆" forState:UIControlStateNormal];
-        [btn setTitle:@"登陆" forState:UIControlStateHighlighted];
-        [btn addTarget:self action:@selector(visitorLogIn) forControlEvents:UIControlEventTouchUpInside];
-        [self.view addSubview:btn];
+    //登陆按钮
+    CGFloat btnW = 100;
+    CGFloat btnH = 30;
+    CGFloat btnX = ([UIScreen mainScreen].bounds.size.width - btnW) / 2;
+    CGFloat btnY = [UIScreen mainScreen].bounds.size.height / 2 + 100;
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(btnX, btnY, btnW, btnH)];
+    [btn setTitleColor: [UIColor orangeColor] forState:UIControlStateNormal];
+    [btn setTitleColor: [UIColor grayColor] forState:UIControlStateHighlighted];
+    [btn setTitle:@"登陆" forState:UIControlStateNormal];
+    [btn setTitle:@"登陆" forState:UIControlStateHighlighted];
+    [btn addTarget:self action:@selector(visitorLogIn) forControlEvents:UIControlEventTouchUpInside];
+    btn.layer.borderWidth = 1;
+    btn.layer.cornerRadius = 5;
+    [self.view addSubview:btn];
 }
 
 - (void)visitorLogIn {
